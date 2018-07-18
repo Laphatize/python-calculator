83c2bae20ebb1dd93fa2ef2ef638e1b0b9c8406a
Pranav Ramesh <laphatize.me@gmail.com>
2018-07-17 20:52:47 -0400

    Fixed an issue
    See issue #1

--- pythoncalc.py
+++ pythoncalc.py
@@ -15,23 +15,31 @@ print("Welcome to Terminal Calculator!")
 
 
 
-name = input("What's your name?: ")
-print("Alrighty, " + name + ". Let's get started.")
+startup = input("First Time? (y/n): ")
+
+if (startup == "y") :
+    print("Enure your numbers are valid and aren't decimals.")
+    
+    print("  ")
+
 
 operator = input("What is the operation you want to do? (*,/,-,+):  ")
 number1 = input("What is the first number?:  ")
 number2 = input("What is the second number?:  ")
 
-if (operator == "*") : final = (int(number1)*int(number2))
-if (operator == "+") : final = (int(number1)+int(number2))
-if (operator == "-") : final = (int(number1)-int(number2)) 
-if (operator == "/") : final = (int(number1)/int(number2)) 
+if (operator == "*") : final = str(int(number1)*int(number2))
+if (operator == "+") : final = str(int(number1)+int(number2))
+if (operator == "-") : final = str(int(number1)-int(number2)) 
+if (operator == "/") : final = str(int(number1)/int(number2)) 
 
 
 
 
-try : print("The final answer should be" + final)
-except : print("I don't think you used valid inputs, please try again!")
+
+#try :
+print("The final answer should be " + final + "!")
+print("Error with calculation? Make sure you let us know in the github repo: https://github.com/Laphatize/python-calculator/issues")
+# except : print("I don't think you used valid inputs, please try again!")
 
 
 
